f980237445e7035cd4e5da5e85460208d15ccbf8
vagrant <vagrant@homestead>
2019-11-07 17:39:03 +0000

    first commit

---
+++ resources/views/layouts/layouts.blade.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        @component('components.header')
+        @endcomponent
+        <div class="container pt-5">
+            @yield('content')
+        </div>
+
+        @component('components.footer')
+        @endcomponent
+
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html> 
